79e143e9d2354cdcb4933544228dcc829b1913f3
Unclesam <adeyemis958@gmail.com>
2024-07-25 13:17:58 -0700

    new

--- clientside/src/Pages/Dashboard/Profile.jsx
+++ clientside/src/Pages/Dashboard/Profile.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import DashboardLayout from '../../Components/Layout/DashboardLayout';
+import Footer from '../../Components/Navigation/Footer';
+import Breadcrumb from '../../Components/Navigation/BreadCrumb';
+
+
 
 
 const Profile = () => {
     return (
         <div>
             <DashboardLayout />
-            <div className="min-h-screen sm:ml-64 pt-20 bg-gray-100 py-10">
-                <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-                    <div className="bg-cover bg-center h-56" style={{ backgroundImage: "url('https://flowbite.com/docs/images/people/profile-picture-5.jpg')" }}></div>
+
+            <div className="min-h-screen sm:ml-64 md:pt-20 bg-gray-100 py-10">
+                <div className="p-4 mt-8">
+                    <Breadcrumb />
+                </div>
+                <div className="max-w-full mx-4 bg-white shadow-lg rounded-lg overflow-hidden">
+                    <div className="bg-cover bg-center h-80 bg-purple-600" 
+                    style={{ backgroundImage: "url('https://flowbite.com/docs/images/people/profile-picture-4.jpg')" }}
+                    ></div>
                     <div className="p-8">
                         <div className="relative flex items-center">
-                            <div className="w-24 h-24 bg-white rounded-full border-4 border-white absolute -top-12 left-1/2 transform -translate-x-1/2">
+                            <div className="w-30 h-30 bg-white rounded-full border-4 border-white absolute -top-28 left-1/2 transform -translate-x-1/2">
                                 <img className="w-full h-full rounded-full object-cover" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="Profile" />
                             </div>
                         </div>
@@ -24,11 +34,11 @@ const Profile = () => {
                             <div className="flex space-x-4">
                                 <div className="text-center">
                                     <h3 className="text-lg font-medium text-gray-800">150</h3>
-                                    <p className="text-gray-600">Posts</p>
+                                    <p className="text-gray-600">Refferals</p>
                                 </div>
                                 <div className="text-center">
                                     <h3 className="text-lg font-medium text-gray-800">300</h3>
-                                    <p className="text-gray-600">Followers</p>
+                                    <p className="text-gray-600">Wallet Balance</p>
                                 </div>
                                 <div className="text-center">
                                     <h3 className="text-lg font-medium text-gray-800">180</h3>
@@ -54,6 +64,9 @@ const Profile = () => {
                             </ul>
                         </div> */}
                     </div>
+                </div>
+                <div className="p-4">
+                    <Footer />
                 </div>
             </div>
         </div>

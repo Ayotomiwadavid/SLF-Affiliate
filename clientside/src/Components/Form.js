962628ddde97fa0bc51aa63b817cf7ca7a185a0c
AKDebug-UX <akoredesalaudeen54@gmail.com>
2024-07-29 19:17:53 +0100

    update PR

--- clientside/src/Components/Form.js
+++ clientside/src/Components/Form.js
@@ -7,7 +7,7 @@ import { toast } from "react-toastify";
 const Form = ({ type }) => {
   const apiUrl = "https://softlife-baxk.onrender.com";
   // const apiUrl = import.meta.env.VITE_API_URL;
-  const { packageList, fetchBalance,
+  const { fetchBalance,
     fetchTransaction,
     fetchReferral,
     fetchPackages,
@@ -252,7 +252,7 @@ const Form = ({ type }) => {
               Choose Package
             </option>
             {packageList.map((pkg) => (
-              <option key={pkg.id} value={pkg.price}>
+              <option key={pkg.id} value={pkg.id}>
                 {pkg.name} ({pkg.price})
               </option>
             ))}
@@ -301,6 +301,36 @@ const Form = ({ type }) => {
 export default Form;
 
 
+let packageList = [
+  {
+    id: 1,
+    description: "Level 1",
+    level: "LEVEL1",
+    name: "Level 1",
+    price: "1000.00"
+  },
+  {
+    id: 2,
+    description: "Level 2",
+    level: "LEVEL2",
+    name: "Level 2",
+    price: "2000.00"
+  },
+  {
+    id: 3,
+    description: "Level 3",
+    level: "LEVEL3",
+    name: "Level 3",
+    price: "3000.00"
+  },
+  {
+    id: 4,
+    description: "Premium",
+    level: "PREMIUM",
+    name: "Premium",
+    price: "5000.00"
+  },
+];
 
 
 // firstName

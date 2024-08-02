78fd0e9e48119ff1269654e09439c0bc4583a328
Techthoth <ayotomiwadavid@proton.me>
2024-08-03 00:48:15 +0100

    saving progress

--- clientside/src/Pages/Dashboard/Deposits.jsx
+++ clientside/src/Pages/Dashboard/Deposits.jsx
@@ -126,8 +126,8 @@ const Deposit = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {recentDeposits.map((deposit, index) => (
             <div className="bg-white p-6 rounded-lg shadow-md" key={index}>
-              <p className="text-gray-700 mb-2">Amount: ${deposit.amount}</p>
-              <p className="text-gray-700 mb-2">Account: {deposit.account}</p>
+              <p className="text-gray-700 mb-2">Amount: {deposit.amount}</p>
+              <p className="text-gray-700 mb-2">Account: <span>&#8358;</span> {deposit.account}</p>
               <p className="text-gray-500">Date: {deposit.date}</p>
             </div>
           ))}
